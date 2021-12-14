d987eddfd5447fe709480fcc19e3d2fadc059ece
deeppde <90175715+deeppde@users.noreply.github.com>
2021-12-14 19:20:51 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -15,7 +15,7 @@ git add -A
 git commit -m 'deploy'
 
 # Push the gh-pages branch
-git push -f git@github.com:winderek/iid-docs.git master:gh-pages
-
+# git push -f git@github.com:winderek/iid-docs.git master:gh-pages
+git push -f git@github.com:SML-SHTU/SML-docs.git master:gh-pages
 # Go back to the previous directory
 cd -
